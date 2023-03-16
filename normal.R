6c690ae937c453f24a5dbe5123ba600f9e9dc080
Nícolas Artifon Dorneles <128085981+NDorneles@users.noreply.github.com>
2023-03-16 16:48:35 -0300

    Add files via upload
    Normal Distribution of x

--- normal.R
+++ normal.R
@@ -1,27 +1,11 @@
-normal <- function(x, mu, sigma, cul){
-  
-  if (cul == 0){ 
-  
-    y <- 1/sigma/sqrt(2*pi)*exp(-0.5*((x-mu)/sigma)**2)
-  
+normal <- function(x, mu = 1, sigma = 0, cul = FALSE) {
+  #Normal distribuction of x. mu stands for mean, sigma stands for standard deviation and cul stands for the acummulative function, where FALSE is the defaul value.
+  if (cul == FALSE) {
+    y <- 1 / sigma / sqrt(2 * pi) * exp(-0.5 * ((x - mu) / sigma)**2)
   }
-  
-  if (cul == 1){
-    
-    source('erro_normal.R')
-    
-    y <- 1/2*(1 + erf((x-mu)/sigma/sqrt(2)))
-    
+  if (cul == TRUE){
+    source('erf.R')
+    y <- 1 / 2 * (1 + erf((x - mu) / sigma / sqrt(2)))
   }
-  
   return(list(x = x, y = y))
 }
-
-x11()
-plot(normal(seq(-5,5,0.05), 0, 1,0))
-x11()
-plot(normal(seq(-5,5,0.05), 0, 1,1))
-
-
-
-
